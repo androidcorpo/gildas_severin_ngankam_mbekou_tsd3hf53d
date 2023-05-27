9dade46be3088cb52af3e982b64f267253727a0e
Mbekou Severin <sgmbekou@gmail.com>
2023-05-27 21:24:21 +0400

    fix:update jpa query

--- src/main/java/fr/anywr/testanywr/infrastructure/repository/StudentRepository.java
+++ src/main/java/fr/anywr/testanywr/infrastructure/repository/StudentRepository.java
@@ -12,14 +12,10 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-  Page<Student> findAll(Pageable pageable);
-  @Query("select s from Student s join s.classRoom c where c.name =?1")
-  Page<Student> findStudentByClassRoomName(String classRoomName, Pageable pageable);
-
   @Query(
-      "select s from Student s join s.classRoom c join c.teacher t where c.name =?1 and (concat(t.firstName, t.lastName) like %?2%)")
+      "select s from Student s join s.classRoom c join c.teacher t where c.name =?1 and (concat(t.firstName,' ', t.lastName) like concat('%',?2,'%'))")
   Page<Student> findStudentByClassRoomNameAndTeacherFullName(String classRoomName, String teacherFullName, Pageable pageable);
   @Query(
-      "select s from Student s join s.classRoom c join c.teacher t where c.name =?1 or (concat(t.firstName, t.lastName) like %?2%)")
+      "select s from Student s join s.classRoom c join c.teacher t where c.name =?1 or (concat(t.firstName, ' ',t.lastName) like concat('%',?2,'%'))")
   Page<Student> findStudentByClassRoomNameOrTeacherFullName(String classRoomName, String teacherFullName, Pageable pageable);
 }
